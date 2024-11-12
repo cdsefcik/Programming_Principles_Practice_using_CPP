b6b9313e2690cc5e0385a7d2a2bc017f33f80c75
Christopher Sefcik <cdsefcik@gmail.com>
2024-11-12 09:39:12 -0500

    Create Chapter_2_Drill_HelloWorld.cpp

---
+++ Chapter_2_Drill_HelloWorld.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+/*
+*This is the first program in the book.
+* I am using the standard library however as opposed to the "std_lib_facilities.h". I use the list of #include files above.
+*/
+
+inline void keep_window_open() {
+	char ch; cin >> ch;
+}
+
+int main() {
+	std::cout << "Hello, World!\n";
+	keep_window_open();
+	return 0;
+};
