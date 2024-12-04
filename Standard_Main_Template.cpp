86c1c30b4c7010f90c2e0d09eafc6808b479f494
Christopher Sefcik <cdsefcik@gmail.com>
2024-12-04 06:32:25 -0500

    Create Standard_Main_Template.cpp

---
+++ Standard_Main_Template.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+/*
+*
+*
+*/
+
+
+
+inline void keep_window_open() {
+	char ch; cin >> ch;
+}
+
+
+
+int main() {         
+
+	
+
+	return EXIT_SUCCESS;
+}
+
+
